b330f4d309e3ad58f24a90e64b98351b86c5f5ce
Bennor McCarthy <bennor.mccarthy@akqa.com>
2023-10-26 14:51:00 +1100

    Added comment to integration

--- apps/web/integration.ts
+++ apps/web/integration.ts
@@ -1,5 +1,12 @@
 import { commerce } from "@commerce/commercetools";
 
+// This initialisation is a dummy example to show how configuration of 
+// vendor specific settings could work. However, it could also likely be managed
+// with environment variables directly in the commercetools adapter code, and that
+// would be preferable.
+//
+// This example also indicates how we could add support for features only supported by one vendor
+// (though that should be a rare exception).
 commerce.init({
   defaultQuantity: 2
 })
