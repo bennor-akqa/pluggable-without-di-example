ea6a4bc9b0c02cd1a07ef13f98ca50c043f50510
Bennor McCarthy <bennor.mccarthy@akqa.com>
2023-10-26 15:03:14 +1100

    Removed commented out code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,16 +3,15 @@ import styles from "./page.module.css";
 
 export default async function Page(): Promise<JSX.Element> {
   const cart = await commerce.getCart("someUserId");
+
   return (
     <main className={styles.main}>
-      {/* <div className={styles.description}> */}
         <h1>Cart</h1>
         {cart.items.map(({ sku, quantity }) => (
           <p key={sku}>
             {sku}: {quantity}
           </p>
         ))}
-      {/* </div> */}
     </main>
   );
 }
